a4feba619f5326d4553bd4c7e722d9c0645dff2f
munna-aziz-hridoy <munna.aziz.hridoy@gmail.com>
2022-05-05 00:41:31 +0800

    create endpoint to get all products

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-// client.connect(() => console.log("DB connected"));
+client.connect(() => console.log("DB connected"));
 const itemsCollection = client.db("warehouse").collection("items");
 
 app.use(express.json());
@@ -23,15 +23,32 @@ app.get("/", (req, res) => {
   res.send("server is connected");
 });
 
-const runGetProduct = async () => {
-  await client.connect();
-  const cursor = itemsCollection.find({});
-  const products = await cursor.toArray();
+const runGetProduct = async (property) => {
+  const { limit, email } = property;
+
+  let cursor = itemsCollection.find({});
+  if (email !== "undefined") {
+    const query = { email };
+    cursor = itemsCollection.find(query);
+  } else {
+    cursor = itemsCollection.find({});
+  }
+
+  let products;
+
+  if (limit !== "undefined") {
+    const limitItem = parseInt(limit);
+    products = cursor.limit(limitItem).toArray();
+  } else {
+    products = cursor.toArray();
+  }
+
   return products;
 };
 
 app.get("/products", async (req, res) => {
-  const products = await runGetProduct().catch();
+  const property = req.query;
+  const products = await runGetProduct(property).catch();
   res.send(products);
 });
 
