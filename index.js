023297140df9eb4e7407cf79312017690be54b39
munna-aziz-hridoy <munna.aziz.hridoy@gmail.com>
2022-05-07 04:16:09 +0800

    create one function to run all the function

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-client.connect(() => console.log("DB connected"));
-const itemsCollection = client.db("warehouse").collection("items");
-
 app.use(express.json());
 app.use(cors());
 
@@ -24,133 +21,144 @@ app.get("/", (req, res) => {
   res.send({ message: "server connected" });
 });
 
-const verifyJWT = (req, res, next) => {
-  const accessToken = req.headers.authorization;
-  if (!accessToken) {
-    res.status(401).send({ message: "Unauthorized Access" });
-    return;
-  }
-  const token = accessToken.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      res.status(403).send({ message: "Forbidden Access" });
+const run = async () => {
+  await client.connect();
+  const itemsCollection = client.db("warehouse").collection("items");
+
+  const verifyJWT = (req, res, next) => {
+    const accessToken = req.headers.authorization;
+    if (!accessToken) {
+      res.status(401).send({ message: "Unauthorized Access" });
       return;
     }
-    req.decoded = decoded;
-    next();
-  });
-};
+    const token = accessToken.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(403).send({ message: "Forbidden Access" });
+        return;
+      }
+      req.decoded = decoded;
+      next();
+    });
+  };
 
-const runGetProduct = async (limit) => {
-  try {
-    const cursor = itemsCollection.find({});
+  const runGetProduct = async (limit) => {
+    try {
+      const cursor = itemsCollection.find({});
 
-    let products;
-    if (limit !== "undefined") {
-      const limitItem = parseInt(limit);
-      products = cursor.limit(limitItem).toArray();
-    } else {
-      products = cursor.toArray();
+      let products;
+      if (limit !== "undefined") {
+        const limitItem = parseInt(limit);
+        products = cursor.limit(limitItem).toArray();
+      } else {
+        products = cursor.toArray();
+      }
+      return products;
+    } finally {
     }
-    return products;
-  } finally {
-  }
-};
+  };
 
-const rungetUserProduct = async (email) => {
-  try {
-    const cursor = itemsCollection.find({ email });
-    const product = await cursor.toArray();
-    return product;
-  } finally {
-  }
-};
+  const rungetUserProduct = async (email) => {
+    try {
+      const cursor = itemsCollection.find({ email });
+      const product = await cursor.toArray();
+      return product;
+    } finally {
+    }
+  };
 
-const runAddProduct = async (product) => {
-  try {
-    const result = await itemsCollection.insertOne(product);
-    return result;
-  } finally {
-  }
-};
+  const runAddProduct = async (product) => {
+    try {
+      const result = await itemsCollection.insertOne(product);
+      return result;
+    } finally {
+    }
+  };
 
-const runGetSingleProduct = async (id) => {
-  try {
-    const query = { _id: ObjectId(id) };
-    const product = await itemsCollection.findOne(query);
-    return product;
-  } finally {
-  }
-};
+  const runGetSingleProduct = async (id) => {
+    try {
+      const query = { _id: ObjectId(id) };
+      const product = await itemsCollection.findOne(query);
+      return product;
+    } finally {
+    }
+  };
 
-const runDeleteProduct = async (id) => {
-  try {
-    const filter = { _id: ObjectId(id) };
-    const result = await itemsCollection.deleteOne(filter);
-    return result;
-  } finally {
-  }
-};
+  const runDeleteProduct = async (id) => {
+    try {
+      const filter = { _id: ObjectId(id) };
+      const result = await itemsCollection.deleteOne(filter);
+      return result;
+    } finally {
+    }
+  };
 
-const runUpdateProduct = async (id, product) => {
-  try {
-    const { quantity, sold } = product;
-    const filter = { _id: ObjectId(id) };
-    const updateDoc = { $set: { quantity: quantity, sold: sold } };
-    const options = { upsert: true };
-    const result = await itemsCollection.updateOne(filter, updateDoc, options);
-    return result;
-  } finally {
-  }
-};
+  const runUpdateProduct = async (id, product) => {
+    try {
+      const { quantity, sold } = product;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = { $set: { quantity: quantity, sold: sold } };
+      const options = { upsert: true };
+      const result = await itemsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      return result;
+    } finally {
+    }
+  };
 
-app.get("/products", async (req, res) => {
-  const limit = req.query.limit;
-  const products = await runGetProduct(limit).catch();
-  res.send(products);
-});
-
-app.get("/singleProduct", async (req, res) => {
-  const id = req.query.id;
-  const product = await runGetSingleProduct(id).catch();
-  res.send(product);
-});
-
-app.get("/myitems", verifyJWT, async (req, res) => {
-  const email = req.query.email;
-  if (email === req.decoded?.email) {
-    const products = await rungetUserProduct(email);
+  app.get("/products", async (req, res) => {
+    const limit = req.query.limit;
+    const products = await runGetProduct(limit).catch();
     res.send(products);
-  } else {
-    res.status(403).send({ message: "unauthorized access" });
-  }
-});
-
-app.post("/products", async (req, res) => {
-  const product = req.body;
-  const result = await runAddProduct(product);
-  res.send(result);
-});
-
-app.delete("/delete/:id", async (req, res) => {
-  const id = req.params.id;
-  const result = await runDeleteProduct(id);
-  res.send(result);
-});
-
-app.put("/update/:id", async (req, res) => {
-  const product = req.body;
-  const id = req.params.id;
-  runUpdateProduct(id, product).catch;
-});
-
-app.post("/getToken", async (req, res) => {
-  const user = req.body;
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "3d",
   });
-  res.send({ accessToken });
-});
+
+  app.get("/singleProduct", async (req, res) => {
+    const id = req.query.id;
+    const product = await runGetSingleProduct(id).catch();
+    res.send(product);
+  });
+
+  app.get("/myitems", verifyJWT, async (req, res) => {
+    const email = req.query.email;
+    if (email === req.decoded?.email) {
+      const products = await rungetUserProduct(email);
+      res.send(products);
+    } else {
+      res.status(403).send({ message: "unauthorized access" });
+    }
+  });
+
+  app.post("/products", async (req, res) => {
+    const product = req.body;
+    const result = await runAddProduct(product);
+    res.send(result);
+  });
+
+  app.delete("/delete/:id", async (req, res) => {
+    const id = req.params.id;
+    const result = await runDeleteProduct(id);
+    res.send(result);
+  });
+
+  app.put("/update/:id", async (req, res) => {
+    const product = req.body;
+    const id = req.params.id;
+    runUpdateProduct(id, product).catch;
+  });
+
+  app.post("/getToken", async (req, res) => {
+    const user = req.body;
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "3d",
+    });
+    res.send({ accessToken });
+  });
+};
+
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log("Server is running :D");
